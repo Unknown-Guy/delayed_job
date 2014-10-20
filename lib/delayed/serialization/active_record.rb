f7845334402430fcdeb55e5ee8d9951c8dee59b6
Unknown_Guy <pamiljans@gmail.com>
2014-10-20 18:02:17 +0300

    fix deserialization for Rails 4.2

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -4,9 +4,14 @@ if defined?(ActiveRecord)
       yaml_as 'tag:ruby.yaml.org,2002:ActiveRecord'
 
       def self.yaml_new(klass, _tag, val)
-        klass.unscoped.find(val['attributes'][klass.primary_key])
+        attribute_name = if ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR >=2
+          'raw_attributes'
+        else
+          'attributes'
+        end
+        klass.unscoped.find(val[attribute_name][klass.primary_key])
       rescue ActiveRecord::RecordNotFound
-        raise Delayed::DeserializationError, "ActiveRecord::RecordNotFound, class: #{klass} , primary key: #{val['attributes'][klass.primary_key]}"
+        raise Delayed::DeserializationError, "ActiveRecord::RecordNotFound, class: #{klass} , primary key: #{val[attribute_name][klass.primary_key]}"
       end
 
       def to_yaml_properties
